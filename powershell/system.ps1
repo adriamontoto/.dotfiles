a6731082e57a2eb7c71db195e6bb80746590b3b4
Adria Montoto <75563346+adriiamontoto@users.noreply.github.com>
2024-03-30 13:38:45 +0100

    feat: create and change directory alias

--- powershell/system.ps1
+++ powershell/system.ps1
@@ -3,6 +3,11 @@ function Set-ParentDirectory {
 	Set-Location ..
 }
 
+function New-SetDirectory([string] $directory_name) {
+    New-Item -ItemType Directory -Path $directory_name
+    Set-Location -Path $directory_name
+}
+
 # System aliases
 Set-Alias -Name .. -Value Set-ParentDirectory
 ${function:...} = { Set-Location ..\.. }
@@ -13,3 +18,5 @@ ${function:de} = { Set-Location ~\Desktop }
 ${function:docs} = { Set-Location ~\Documents }
 ${function:p} = { Set-Location ~\Documents\sync\projects }
 ${function:dw} = { Set-Location ~\Downloads }
+
+Set-Alias -Name mkd -Value New-SetDirectory
