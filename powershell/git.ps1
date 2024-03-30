48dd4477ab8b0fba9d4f08ac63cea96c3684e577
Adria Montoto <75563346+adriiamontoto@users.noreply.github.com>
2024-03-30 20:57:24 +0100

    fix: when using pull and push is common to use arguments

--- powershell/git.ps1
+++ powershell/git.ps1
@@ -5,8 +5,8 @@
 ${function:gs} = { git status }
 ${function:ga} = { git add $args }
 ${function:gc} = { git commit -m $args }
-${function:gpull} = { git pull }
-${function:gpush} = { git push }
+${function:gpull} = { git pull $args }
+${function:gpush} = { git push $args }
 ${function:glog} = { git log --oneline --decorate --color }
 ${function:gammend} = { git commit --amend --no-edit }
 ${function:gundo} = { git reset --soft HEAD~1 }
