8824b6757898ce58a8386078a05266fb283de88c
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2024-07-08 09:38:51 +0200

    feat: add a command to modify last commit message

--- powershell/git.ps1
+++ powershell/git.ps1
@@ -10,5 +10,6 @@ ${function:gpush} = { git push $args }
 ${function:gforce} = { git push --force-with-lease $args }  # Force push, but only if the remote branch is the same as the local branch
 ${function:glog} = { git log --oneline --decorate --color }
 ${function:gamend} = { git commit --amend --no-edit }  # Amend the last chnage to the last commit
+${function:grewrite} = { git commit --amend }  # Modify the last commit message
 ${function:gundo} = { git reset --soft HEAD~1 }  # Undo last commit
 ${function:gnuke} = { git clean -df; git reset --hard }  # Removes all untracked files and reset the repo to the last commit
