e46c564f4b4f9c0c92959894d62995481950fb7e
Adria Montoto <75563346+adriamontoto@users.noreply.github.com>
2025-03-04 08:28:52 +0100

    ✨ feat: drop powershell support

--- shells/powershell/Microsoft.PowerShell_profile.ps1
+++ shells/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,1 @@
 oh-my-posh init pwsh --config "${env:POSH_THEMES_PATH}/robbyrussell.omp.json" | Invoke-Expression
-
-Get-ChildItem -Path "${HOME}/.dotfiles/shells/aliases" -Recurse -Filter "*.ps1" | ForEach-Object {
-    . $_.FullName
-}
